adaa2cdcb93116d27ac7966864e8372f5d622741
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2014-08-28 10:56:25 -0400

    This commit fulfills the full spec, but it does not include styling.  The comment submission page does not redirect properly in this version.

--- schema.sql
+++ schema.sql
@@ -1,13 +1,15 @@
 CREATE TABLE articles (
   id serial PRIMARY KEY,
-  name varchar(255),
-  description varchar(300),
-  url varchar(60)
+  name varchar(255) NOT NULL,
+  description varchar(300) NOT NULL,
+  url varchar(60) NOT NULL
 );
 
 
 CREATE TABLE comments (
   id serial PRIMARY KEY,
-  commenter varchar(55),
-  contents varchar(80)
+  commenter varchar(55) NOT NULL,
+  contents varchar(80) NOT NULL,
+  reference_article integer references articles(id),
+  reference_comment integer
   );
--- server.rb
+++ server.rb
@@ -20,13 +20,22 @@ end
 
 
 def url_exists_in_file?(url)
-  articles = pull_articles("articles.csv")
+  # articles = pull_articles("articles.csv")
 
-  articles.each do |article|
-    if article["url"] == url
-      return true
+  # articles.each do |article|
+  #   if article["url"] == url
+  #     return true
+  #   end
+  # end
+    db_connection do |conn|
+      @articles = conn.exec('SELECT * FROM articles WHERE url = $1;', [url])
     end
-  end
+
+    @articles.each do |article|
+      if article["url"] == url
+        return true
+      end
+    end
 
   false
 end
@@ -53,34 +62,28 @@ def validation_results(name, url, description)
     error_messages << "The description was left blank.  Please input a description."
   end
 
-  if description_test.length > 20
-    error_messages << "The description field was greater than 20 characters.  Please input a description that is twenty characters or less (not including spaces)."
+  if description_test.length < 20
+    error_messages << "The description field was less than 20 characters.  Please input a description that is twenty characters or less (not including spaces)."
   end
 
   error_messages
 end
 
 
-# def valid_name?(name)
+def validation_comments(commenter,description_comment)
 
-#   if name.length == 0
-#     return false
-#   else
-#     return true
-#   end
-# end
+  error_messages = []
 
-# def valid_url?(url)
+  if commenter == nil || commenter.length == 0
+    error_messages << "You did not fill out your name.  Please fill in your name."
+  end
 
-#   if url.length == 0
-#     return false
-#   end
+  if description_comment == nil || description_comment.length == 0
+    error_messages << "You did not type anything in the comment field.  Please fill in the comment."
+  end
 
-#   if
-
-
-
-# end
+  error_messages
+end
 
 
 
@@ -113,7 +116,7 @@ post '/submit' do
   url = params["article_url"]
   description = params["article_description"]
   error_messages = []
-  # error_messages = validation_results(name, url, description)
+  error_messages = validation_results(name, url, description)
 
   if error_messages.length == 0
     db_connection do |conn|
@@ -134,7 +137,50 @@ post '/submit' do
 
 end
 
+get '/comments/:id' do
 
+  @id = params[:id]
+  @commenter = ''
+  @description = ''
+  @error_messages = []
+
+  db_connection do |conn|
+      @article = conn.exec('SELECT * FROM articles WHERE id = $1', [@id])
+      @comments = conn.exec('SELECT * FROM comments')
+
+  end
+
+  erb :comments
+end
+
+post '/comments/:id' do
+  commenter = params["commenter"]
+  description_comment = params["description_comment"]
+  error_messages = validation_comments(commenter,description_comment)
+  @id = params[:id]
+
+
+  if error_messages == nil || error_messages.length == 0
+    db_connection do |conn|
+      conn.exec('INSERT INTO comments (commenter, contents, reference_article) VALUES ($1, $2, $3);', [commenter, description_comment, @id.to_i])
+    end
+
+  else
+    @commenter = commenter
+    @description_comment = description_comment
+    @error_messages = error_messages
+
+
+    db_connection do |conn|
+      @article = conn.exec('SELECT * FROM articles WHERE id = $1', [@id])
+      @comments = conn.exec('SELECT * FROM comments')
+    end
+
+    redirect '/'
+  end
+
+
+end
 
 
 
